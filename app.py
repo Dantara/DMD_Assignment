87e2b8225b276ed7a5bbcaad252e6391ecc3781f
Dantara <supercreed2@gmail.com>
2019-11-28 14:29:40 +0300

    Routes

--- app.py
+++ app.py
@@ -1,33 +1,21 @@
 import hug
 
 from support.templates import get_template
-from controllers import queryController
+from controllers.queryController import QueryController
+
+router = hug.route.API(__name__)
+
+router.get('/query1')(QueryController.query1)
+router.get('/query2')(QueryController.query2)
+router.get('/query3')(QueryController.query3)
+router.get('/query4')(QueryController.query4)
+router.get('/query5')(QueryController.query5)
+
+@hug.static('/static')
+def static():
+    return ['./static']
 
 @hug.get('/', output=hug.output_format.html)
 def root():
     template = get_template("index.html")
     return template.render({})
-
-@hug.get('/query1')
-def q1():
-    return queryController.query1()
-
-@hug.get('/query2')
-def q2():
-    return queryController.query2()
-
-@hug.get('/query3')
-def q3():
-    return queryController.query3()
-
-@hug.get('/query4')
-def q4():
-    return queryController.query4()
-
-@hug.get('/query5')
-def q5():
-    return queryController.query5()
-
-@hug.static('/static')
-def static():
-    return ['./static']
--- controllers/queryController.py
+++ controllers/queryController.py
@@ -1,19 +1,20 @@
-def query1():
-    print('query1')
-    return {"data": "query1"}
+class QueryController:
+    def query1():
+        print('query1')
+        return {"data": "query1"}
 
-def query2():
-    print('query2')
-    return {"data": "query2"}
+    def query2():
+        print('query2')
+        return {"data": "query2"}
 
-def query3():
-    print('query3')
-    return {"data": "query3"}
+    def query3():
+        print('query3')
+        return {"data": "query3"}
 
-def query4():
-    print('query4')
-    return {"data": "query4"}
+    def query4():
+        print('query4')
+        return {"data": "query4"}
 
-def query5():
-    print('query5')
-    return {"data": "query5"}
+    def query5():
+        print('query5')
+        return {"data": "query5"}
